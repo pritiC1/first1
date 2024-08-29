c74dcb229ca4f210d0c9af5de53a8f4ff7a3f69f
pritiC1 <pritichaugule44@gmail.com>
2024-08-29 15:58:45 +0530

    Add files via upload

---
+++ new1.py
@@ -0,0 +1,7 @@
+a,b,c=(int(x) for x in input("enter values").split(","))
+if(a>b and a>c):
+    print("a is greater")
+if(b>a and b>c):
+    print("b is greater")
+if(c>a and c>b):
+    print("c is greater")    
